8b8b9e24a2be7b3e315db57f140f63e581b06c57
naqi <2080252788@qq.com>
2019-03-25 15:02:54 +0800

    快排

--- src/main/java/com/example/demo/practice/QuickSort.java
+++ src/main/java/com/example/demo/practice/QuickSort.java
@@ -2,6 +2,12 @@ package com.example.demo.practice;
 
 /**
  * 快排
+ *
+ * 先构建十条io流，分别指向十个文件，每条io流读取对应文件的第一条数据，然后比较时间戳，选择出时间戳最小的那条数据，
+ * 将其写入一个新的文件，然后指向该时间戳的io流读取下一行数据，然后继续刚才的操作，比较选出最小的时间戳数据，写入新文件，
+ * io流读取下一行数据，以此类推，完成文件的合并， 这种处理方式，日志文件有n个数据就要比较n次，每次比较选出一条数据来写入，
+ * 时间复杂度是O（n），空间复杂度是O（1）,几乎不占用内存！
+
  * Created by Naq on 2019/3/22.
  */
 public class QuickSort {
@@ -20,6 +26,7 @@ public class QuickSort {
         quickSortInternally(a, q + 1, r);
     }
 
+    // 分区函数
     private static int partition(int[] a, int p, int r) {
         int pivot = a[r];
         int i = p;
