2e7667c274bd7c731b3db2fefbbdbeec001b88f1
naqi <2080252788@qq.com>
2019-03-25 11:08:28 +0800

    快排

--- src/main/java/com/example/demo/practice/QuickSort.java
+++ src/main/java/com/example/demo/practice/QuickSort.java
@@ -6,4 +6,42 @@ package com.example.demo.practice;
  */
 public class QuickSort {
 
+    // 快速排序，a是数组，n表示数组的大小
+    public static void quickSort(int[] a, int n) {
+        quickSortInternally(a, 0, n - 1);
+    }
+
+    // 快速排序递归函数，p,r为下标
+    private static void quickSortInternally(int[] a, int p, int r) {
+        if (p >= r) return;
+        // 获取分区点
+        int q = partition(a, p, r);
+        quickSortInternally(a, p, q - 1);
+        quickSortInternally(a, q + 1, r);
+    }
+
+    private static int partition(int[] a, int p, int r) {
+        int pivot = a[r];
+        int i = p;
+        for (int j = p; j < r; ++j) {
+            if (a[j] < pivot) {
+                if (i == j) {
+                    ++i;
+                } else {
+                    int tmp = a[i];
+                    a[i++] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+
+        // 交换
+        int tmp = a[i];
+        a[i] = a[r];
+        a[r] = tmp;
+
+        System.out.println("i=" + i);
+        // 返回合适的下标，作为分区标识
+        return i;
+    }
 }
