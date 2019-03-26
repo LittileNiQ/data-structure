42a8b57aa0876a0e185bac45741e099f87de6a41
naqi <2080252788@qq.com>
2019-03-26 17:31:47 +0800

    二分查找

---
+++ src/main/java/com/example/demo/practice/BinarySearch.java
@@ -0,0 +1,47 @@
+package com.example.demo.practice;
+
+/**
+ * 二分查找(用最省内存的方式实现快速查找功能)
+ * Created by Naq on 2019/3/26.
+ */
+public class BinarySearch {
+
+    // 二分查找的递归实现
+    public int bsearchRecursive(int[] a, int n, int val) {
+        return bsearchInternally(a, 0, n - 1, val);
+    }
+
+    private int bsearchInternally(int[] a, int low, int high, int value) {
+        if (low > high) return -1;
+        int mid = low + ((high - low) >> 1);
+        if (a[mid] == value) {
+            return mid;
+        } else if (a[mid] < value) {
+            return bsearchInternally(a, mid + 1, high, value);
+        } else {
+            return bsearchInternally(a, low, mid - 1, value);
+        }
+    }
+
+    // 二分查找非递归
+    public int bsearch(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+
+            if (a[mid] == value) {
+                return mid;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+
+}
