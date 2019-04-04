7bac670ce8f1dde415283c71c5f7eb3b2d034ee2
naqi <2080252788@qq.com>
2019-04-04 14:04:25 +0800

    利用堆实现有界数组的topK(前k大)元素查找问题

---
+++ src/main/java/com/example/demo/study/Median.java
@@ -0,0 +1,8 @@
+package com.example.demo.study;
+
+/**
+ * 数组利用堆求中位数
+ * Created by Naq on 2019/4/4.
+ */
+public class Median {
+}
---
+++ src/main/java/com/example/demo/study/TopKcount.java
@@ -0,0 +1,56 @@
+package com.example.demo.study;
+
+/**
+ * Created by Naq on 2019/4/4.
+ */
+
+import java.util.PriorityQueue;
+
+/**
+ * 进行求topK的问题:
+ * { 如何在一个包含+n+个数据的数组中，查找前+K+大数据呢？我们可以维护一个大小为+K+的小顶堆，
+ * 顺序遍历数组，从数组中取出取数据与堆顶元素比较。如果比堆顶元素大，我们就把堆顶元素删除，
+ * 并且将这个元素插入到堆中；如果比堆顶元素小，则不做处理，继续遍历数组。
+ * 这样等数组中的数据都遍历完之后，堆中的数据就是前+K+大数据了 }
+ *
+ * 利用PriorityQueue
+ * Created by Naq on 2019/4/4.
+ */
+public class TopKcount {
+
+    /**
+     * 求数据中前K大数据
+     *
+     * @param data
+     * @param k
+     * @return
+     */
+    public int[] topk(int[] data, int k) {
+
+        // 构建一个基于优先级堆的无界队列（相当于构建一个k大小的小根堆）
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k);
+
+        for (int i = 0; i < data.length; i++) {
+            if (queue.size() < k) {
+                queue.offer(data[i]);
+            } else {
+                int value = queue.peek();
+                // 如果发现数据比堆顶元素大，则加入到小顶堆中
+                if (data[i] > value) {
+                    // 删除堆顶元素并替换为data[i]
+                    queue.poll();
+                    queue.offer(data[i]);
+                }
+            }
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+        // 遍历完成后，小顶堆的数据就为需要求得的topk的数据
+        while (!queue.isEmpty()) {
+            result[index++] = queue.poll();
+        }
+
+        return result;
+    }
+}
