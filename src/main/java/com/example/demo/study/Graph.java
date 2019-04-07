d0a5419e4eb7305f22268de76151b6de490021b6
naqi <2080252788@qq.com>
2019-04-07 17:16:54 +0800

    图

---
+++ src/main/java/com/example/demo/study/Graph.java
@@ -0,0 +1,26 @@
+package com.example.demo.study;
+
+import java.util.LinkedList;
+
+/**
+ * 邻接表存储无向图
+ * Created by Naqi on 2019/4/6.
+ */
+public class Graph { // 无向图
+    private int v; // 顶点的个数
+    private LinkedList<Integer> adj[]; // 邻接表
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int s, int t) { // 无向图一条边存两次
+        adj[s].add(t);
+        adj[t].add(s);
+    }
+}
+
