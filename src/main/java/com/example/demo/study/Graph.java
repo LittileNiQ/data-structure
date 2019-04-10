75e4024401d75a20ed208fe7b5832c6b9624f2ad
naqi <2080252788@qq.com>
2019-04-10 22:23:50 +0800

    图的遍历

--- src/main/java/com/example/demo/study/Graph.java
+++ src/main/java/com/example/demo/study/Graph.java
@@ -1,6 +1,7 @@
 package com.example.demo.study;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 邻接表存储无向图
@@ -22,5 +23,72 @@ public class Graph { // 无向图
         adj[s].add(t);
         adj[t].add(s);
     }
+
+    public void bfs(int s, int t) {
+        if (s == t) return;
+        // 已经被访问的顶点，用来避免顶点被重复访问。如果顶点 q 被访问，那相应的 visited[q] 会被设置为 true。
+        boolean[] visited = new boolean[v];
+        visited[s]=true;
+        // 用来存储已经被访问、但相连的顶点还没有被访问的顶点
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        // 用来记录搜索路径
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < adj[w].size(); ++i) {
+                int q = adj[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                    visited[q] = true;
+                    queue.add(q);
+                }
+            }
+        }
+    }
+
+    boolean found = false; // 全局变量或者类成员变量
+
+    public void dfs(int s, int t) {
+        found = false;
+        boolean[] visited = new boolean[v];
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        recurDfs(s, t, visited, prev);
+        print(prev, s, t);
+    }
+
+    private void recurDfs(int w, int t, boolean[] visited, int[] prev) {
+        if (found == true) return;
+        visited[w] = true;
+        if (w == t) {
+            found = true;
+            return;
+        }
+        for (int i = 0; i < adj[w].size(); ++i) {
+            int q = adj[w].get(i);
+            if (!visited[q]) {
+                prev[q] = w;
+                recurDfs(q, t, visited, prev);
+            }
+        }
+    }
+
+    private void print(int[] prev, int s, int t) { // 递归打印 s->t 的路径
+        if (prev[t] != -1 && t != s) {
+            print(prev, s, prev[t]);
+        }
+        System.out.print(t + " ");
+    }
+
 }
 
