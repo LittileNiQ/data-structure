98f4388de0336af435488fcd655ab780844d817b
naqi <2080252788@qq.com>
2019-05-05 18:05:28 +0800

    RK算法

---
+++ src/main/java/com/example/demo/study/RK.java
@@ -0,0 +1,20 @@
+package com.example.demo.study;
+
+/**
+ * RK算法
+ * https://www.cnblogs.com/zghaobac/p/3999253.html
+ * https://blog.csdn.net/xlxxcc/article/details/64125265
+ * Created by Naq on 2019/5/5.
+ */
+public class RK {
+
+    public static void main(String[] args) {
+        String a = "qwertyuioplkjhgfdsa";
+        String b = "polk";
+
+    }
+
+    public int match() {
+        return 1;
+    }
+}
